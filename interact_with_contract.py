6ffb6dfadd8d4d862d0f0188007f910214c61aa0
Eisuke15 <hideyu41691@gmail.com>
2023-04-11 18:47:56 +0900

    simulate method

--- interact_with_contract.py
+++ interact_with_contract.py
@@ -1,7 +1,6 @@
 import json
 
-from web3 import Web3
-from web3.logs import STRICT, IGNORE, DISCARD, WARN
+from worker import Worker
 
 
 abi_file = "FederatedLearning.json"
@@ -10,35 +9,6 @@ contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 # Load the contract's ABI
 with open(abi_file) as f:
     contract_abi = json.load(f)['abi']
-
-
-class Worker:
-    def __init__(self, index, w3: Web3) -> None:
-        self.index = index
-        self.w3 = w3
-
-
-def init_worker(i):
-    w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-
-    account = w3.eth.accounts[i]
-    w3.eth.default_account = account
-    return Worker(i, w3)
-
-
-def register(worker: Worker):
-    # Get the contract object
-    contract = worker.w3.eth.contract(abi=contract_abi, address=contract_address)
-
-    # https://web3py.readthedocs.io/en/v5/contracts.html#web3.contract.ContractFunction.transact
-    tx_hash = contract.functions.register().transact()
-    print(f"worker {worker.index} registered")
-
-    tx_receipt = worker.w3.eth.get_transaction_receipt(tx_hash)
-    processed_logs = contract.events.LearningRightGranted().process_receipt(tx_receipt)
-
-    return processed_logs
-
 
 
 def watch_event(worker: Worker):
@@ -50,30 +20,23 @@ def watch_event(worker: Worker):
         print(event)
 
 
-def submit_model(worker, model_index):
-    contract = worker.w3.eth.contract(abi=contract_abi, address=contract_address)
-    import datetime
-    tx_hash = contract.functions.submitModel(str(model_index), [] if model_index == 0 else [str(model_index - 1)]).transact()
-    print(f"worker {worker.index} submitted model")
-    tx_receipt = worker.w3.eth.get_transaction_receipt(tx_hash)
-    processed_logs = contract.events.LearningRightGranted().process_receipt(tx_receipt, errors=IGNORE)
-
-    return processed_logs
-
-
 def simulate():
-    workers = [init_worker(i) for i in range(10)]
+    workers = [Worker(i, contract_abi, contract_address) for i in range(10)]
     for w in workers:
-        logs = register(w)
-
-    for i in range(5):
-        event_args = logs[-1]['args']
+        tx_hash = w.register
+        tx_receipt = w.w3.eth.get_transaction_receipt(tx_hash)
+        logs = w.contract.events.LearningRightGranted().process_receipt(tx_receipt)
+    
+    for i in range(10):
+        event = logs[-1]
         for w in workers:
-            if w.w3.eth.default_account == event_args['client']:
-                next_worker = w
+            if w.w3.eth.default_account == event['args']['client']:
+                worker = w
+                print(f"next worker is {w.index}")
                 break
-        model_index = event_args['modelIndex']
-        logs = submit_model(next_worker, model_index)
+        tx_hash = worker.handle_event(event)
+        tx_receipt = worker.w3.eth.get_transaction_receipt(tx_hash)
+        logs = worker.contract.events.LearningRightGranted().process_receipt(tx_receipt)
 
 
 if __name__ == "__main__":
