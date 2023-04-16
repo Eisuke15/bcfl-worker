337f098a46d67fa67758d4a0311f2bb2f319eac9
Eisuke15 <hideyu41691@gmail.com>
2023-04-17 01:24:37 +0900

    specify progress bar

--- training.py
+++ training.py
@@ -6,10 +6,10 @@ from tqdm import tqdm
 from net import Net
 
 
-def train(model: Net, optimizer: Adam, device: torch.device, criterion: torch.nn.CrossEntropyLoss, train_loader: DataLoader, num_epochs: int):
+def train(model: Net, optimizer: Adam, device: torch.device, criterion: torch.nn.CrossEntropyLoss, train_loader: DataLoader, num_epochs: int, progress_bar: bool = True):
     model.train()
     for epoch in range(num_epochs):
-        for (inputs, labels) in tqdm(train_loader, desc=f"epoch:{epoch+1} training", leave=False):
+        for (inputs, labels) in (tqdm(train_loader, desc=f"epoch:{epoch+1} training", leave=False) if progress_bar else train_loader):
             inputs, labels = inputs.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
@@ -18,18 +18,17 @@ def train(model: Net, optimizer: Adam, device: torch.device, criterion: torch.nn
             optimizer.step()
 
 
-def test(model: Net, device: torch.device, test_loader: DataLoader):
+def test(model: Net, device: torch.device, test_loader: DataLoader, progress_bar: bool = True):
     sum_correct = 0
     model.eval()
 
-    for (inputs, labels) in tqdm(test_loader, desc=f"testing", leave=False):
+    for (inputs, labels) in (tqdm(test_loader, desc=f"testing", leave=False) if progress_bar else test_loader):
         inputs, labels = inputs.to(device), labels.to(device)
         outputs = model(inputs)
         _, predicted = outputs.max(1)
         sum_correct += (predicted == labels).sum().item()
     
     accuracy = float(sum_correct/len(test_loader.dataset))
-    print(f"test accuracy={accuracy}")
     return accuracy
     
     
