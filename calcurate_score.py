694e9464cd3364afa871ab573d49ab5b5d2cf181
Eisuke15 <hideyu41691@gmail.com>
2023-04-19 03:58:29 +0900

    save accuracy list

--- calcurate_score.py
+++ calcurate_score.py
@@ -1,16 +1,23 @@
 from config import CONTRACT_ABI, CONTRACT_ADDRESS
 from training import test, testset, trainset
 from worker import Worker
+import torch
 
 worker = Worker(0, CONTRACT_ABI, CONTRACT_ADDRESS, trainset, testset, gpu_num=1)
 cids = [worker.contract.functions.initialModelCID().call()]
+
+acc_list = []
 for i in range(10000):
     try:
-        cids.append(worker.contract.functions.models(i).call()[0])
+        cid, author_index = worker.contract.functions.models(i).call()
+        cids.append(cid)
     except Exception: 
         break
 
-for latest_model_index in range(5, len(cids)):
-    worker.aggregate(cids[latest_model_index - 5:latest_model_index])
+for latest_model_index in range(len(cids)):
+    worker.aggregate(cids[max(0, latest_model_index - 10):latest_model_index])
     acc = test(model=worker.net, test_loader=worker.test_loader, device=worker.device, progress_bar=True)
+    acc_list.append(acc)
     print(f'{latest_model_index}: {acc}')
+
+torch.save(acc_list, 'graph_cifar10/acc_list.pth')
