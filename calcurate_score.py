a965597b92c3d25f9ef490558fbaa2d277834e82
Eisuke15 <hideyu41691@gmail.com>
2023-05-11 01:53:18 +0900

    calcurate aggregated model score

--- calcurate_score.py
+++ calcurate_score.py
@@ -1,22 +1,27 @@
+import torch
+from torch.utils.data import DataLoader
+
 from config import CONTRACT_ABI, CONTRACT_ADDRESS
 from training import test, testset, trainset
 from worker import Worker
-import torch
 
-worker = Worker(0, CONTRACT_ABI, CONTRACT_ADDRESS, trainset, testset, gpu_num=1)
+worker = Worker(0, CONTRACT_ABI, CONTRACT_ADDRESS, trainset, testset, gpu_num=0)
 cids = [worker.contract.functions.initialModelCID().call()]
 
+test_loader = DataLoader(testset, batch_size = 128, shuffle = True, num_workers = 2, pin_memory=True)
+
 acc_list = []
-for i in range(10000):
+for i in range(2000):
     try:
         cid, author_index = worker.contract.functions.models(i).call()
         cids.append(cid)
+        print(cid)
     except Exception: 
         break
 
 for latest_model_index in range(len(cids)):
     worker.aggregate(cids[max(0, latest_model_index - 10):latest_model_index])
-    acc = test(model=worker.net, test_loader=worker.test_loader, device=worker.device, progress_bar=True)
+    acc = test(model=worker.net, test_loader=test_loader, device=worker.device, progress_bar=True)
     acc_list.append(acc)
     print(f'{latest_model_index}: {acc}')
 
