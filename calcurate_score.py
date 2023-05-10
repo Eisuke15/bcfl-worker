d10b3a359e15d810ea12f18bf1391a86b2f6e385
Eisuke15 <hideyu41691@gmail.com>
2023-05-11 04:28:45 +0900

    args

--- calcurate_score.py
+++ calcurate_score.py
@@ -5,7 +5,7 @@ from config import CONTRACT_ABI, CONTRACT_ADDRESS
 from training import test, testset, trainset
 from worker import Worker
 
-worker = Worker(0, CONTRACT_ABI, CONTRACT_ADDRESS, trainset, testset, gpu_num=0)
+worker = Worker(0, CONTRACT_ABI, CONTRACT_ADDRESS, trainset, progress_bar=True)
 cids = [worker.contract.functions.initialModelCID().call()]
 
 test_loader = DataLoader(testset, batch_size = 128, shuffle = True, num_workers = 2, pin_memory=True)
