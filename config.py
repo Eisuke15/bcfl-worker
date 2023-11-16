88f5d97cc4bc93351cbc3b7147a6729825a1d1c3
Eisuke15 <hideyu41691@gmail.com>
2023-11-16 17:52:52 +0900

    move abi file

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import json
 
-abi_file = "FederatedLearning.json"
+abi_file = "abi/FederatedLearning.json"
 
 # Load the contract's ABI
 with open(abi_file) as f:
-    CONTRACT_ABI = json.load(f)['abi']
+    CONTRACT_ABI = json.load(f)
 
 CONTRACT_ADDRESS ="0x5FbDB2315678afecb367f032d93F642f64180aa3"
