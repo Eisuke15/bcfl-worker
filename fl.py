7707adbe12b406f65b6b1381b860aa655018901e
Eisuke15 <hideyu41691@gmail.com>
2023-12-06 22:47:29 +0900

    fix logging

--- fl.py
+++ fl.py
@@ -14,8 +14,7 @@ from common.training import test, testset, train, trainset
 torch.backends.cudnn.benchmark = True
 
 parser = ArgumentParser()
-parser.add_argument('filter', type=str, help='what dataset to use')
-parser.add_argument('-n', '--n-round', type=int, help="number of rounds to train for", default=100)
+parser.add_argument('-n', '--n-round', type=int, help="number of rounds to train for", default=100000)
 parser.add_argument('-g', '--gpu-num', type=int, help='what gpu to use', default=0)
 args = parser.parse_args()
 
@@ -23,7 +22,7 @@ n_node = 10
 
 device = torch.device(f"cuda:{args.gpu_num}" if torch.cuda.is_available() else "cpu")
 
-filter = args.filter
+filter = 'noniid'
 indices=torch.load(f'./indices/{filter}.pt')
 
 
@@ -34,41 +33,38 @@ test_loader = DataLoader(testset, batch_size = 256, shuffle = False, num_workers
 models = [Net().to(device) for _ in range(n_node)]
 optimizers = [Adam(model.parameters()) for model in models]
 
-accuracy = [[] for _ in range(n_node + 1)] 
-
 global_model = Net().to(device).state_dict()
 
-for round in range(args.n_round):
-    print(f"Round {round+1}")
+with open(f'logs/fl_{filter}.log', 'w') as f:
+    for round in range(args.n_round):
+        print(f"Round {round+1}")
 
-    for i, (model, optimizer, train_loader) in enumerate(zip(models, optimizers, train_loaders)):
-        print(f"Worker {i}")
-        model.load_state_dict(global_model)
+        for i, (model, optimizer, train_loader) in enumerate(zip(models, optimizers, train_loaders)):
+            print(f"Worker {i}")
+            model.load_state_dict(global_model)
 
-        # train models
-        train(model=model, optimizer=optimizer, device=device, train_loader=train_loader, num_epochs=5)
+            # train models
+            train(model=model, optimizer=optimizer, device=device, train_loader=train_loader, num_epochs=5)
 
-        # test models
-        acc = test(model=model, device=device, test_loader=test_loader)
-        accuracy[i].append(acc)
-        print(f"Worker {i} accuracy: {acc}")
+            # # test models
+            # acc = test(model=model, device=device, test_loader=test_loader)
+            # accuracy[i].append(acc)
+            # print(f"Worker {i} accuracy: {acc}")
 
-    new_global_model = global_model.copy()
+        new_global_model = global_model.copy()
 
-    # aggregate models
-    for model in models:
-        model_parameters = model.state_dict()
-        for key in new_global_model:
-            new_global_model[key] = new_global_model[key] + (model_parameters[key] - global_model[key]) / n_node
-   
-    global_model = new_global_model
+        # aggregate models
+        for model in models:
+            model_parameters = model.state_dict()
+            for key in new_global_model:
+                new_global_model[key] = new_global_model[key] + (model_parameters[key] - global_model[key]) / n_node
+    
+        global_model = new_global_model
 
-    # test global model
-    global_model_test = Net().to(device)
-    global_model_test.load_state_dict(global_model)
-    acc = test(model=global_model_test, device=device, test_loader=test_loader)
-    accuracy[-1].append(acc)
-    print(f"Global model accuracy: {acc}")
-
-torch.save(accuracy, f'graph/fl_{filter}.pt')
-        
+        # test global model
+        global_model_test = Net().to(device)
+        global_model_test.load_state_dict(global_model)
+        acc = test(model=global_model_test, device=device, test_loader=test_loader)
+        print(acc, file=f)
+        f.flush()
+        print(f"Global model accuracy: {acc}")
